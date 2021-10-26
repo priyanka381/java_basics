c70ea57b26da1ef82cc1644d7c208f523369a541
DELL <DELL@DESKTOP-U22BJGJ>
2021-10-26 12:34:10 +0530

    array list

--- java basics/src/collections/ArrayListExample2.java
+++ java basics/src/collections/ArrayListExample2.java
@@ -4,14 +4,14 @@ import java.util.ArrayList;
 
 public class ArrayListExample2 {
 	public static void main(String[] args) {
-		ArrayList<String> list = new ArrayList<String>();
-		list.add("apple");
-		list.add("grapes");
-		list.add("mango");
-		list.add("banana");
-		System.out.println("Returing element: "+list.get(1));
-		list.set(1,"dates");
-		for(String fruit:list)
+		ArrayList<String> al = new ArrayList<String>();
+	      al.add("apple");
+		  al.add("grapes");
+		  al.add("mango");
+		  al.add("banana");
+		System.out.println("Returing element: "+al.get(1));
+		al.set(1,"dates");
+		for(String fruit:al)
 			System.out.println(fruit);
 			
 	}
