3037f52fb9bf84334f82f98b6494a0a28ba16bd3
DELL <DELL@DESKTOP-U22BJGJ>
2021-10-26 11:24:39 +0530

    collections

---
+++ java basics/src/collections/DemoArrayList.java
@@ -0,0 +1,15 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DemoArrayList {
+	public static void main(String[] args) {
+		List names = new ArrayList<String>();
+		names.add("priya");
+		names.add(40);
+		System.out.println(names);
+		
+	}
+
+}
