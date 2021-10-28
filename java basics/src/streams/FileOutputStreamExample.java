dc0b8cd4bbdf166979365bc96efb2287c1c745bf
DELL <DELL@DESKTOP-U22BJGJ>
2021-10-28 15:22:00 +0530

    input, output streams

--- java basics/src/streams/FileOutputStreamExample.java
+++ java basics/src/streams/FileOutputStreamExample.java
@@ -5,7 +5,7 @@ import java.io.FileOutputStream;
 public class FileOutputStreamExample {
 		    public static void main(String args[]){    
 	           try{    
-	             FileOutputStream foutpipe=new FileOutputStream("D:\\\\testout.txt");    
+	             FileOutputStream foutpipe=new FileOutputStream("D:\\ \\testout.txt");    
 	             foutpipe.write(68);    
 	             foutpipe.close();    
 	             System.out.println("success...");    
