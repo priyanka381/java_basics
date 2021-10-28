9e9bc484cf9d5d899795f96dae6b79610777882c
DELL <DELL@DESKTOP-U22BJGJ>
2021-10-28 12:32:38 +0530

    streams

---
+++ java basics/src/streams/FileOutputStreamExample.java
@@ -0,0 +1,15 @@
+package streams;
+
+import java.io.FileOutputStream;
+
+public class FileOutputStreamExample {
+		    public static void main(String args[]){    
+	           try{    
+	             FileOutputStream foutpipe=new FileOutputStream("D:\\\\testout.txt");    
+	             foutpipe.write(68);    
+	             foutpipe.close();    
+	             System.out.println("success...");    
+	            }catch(Exception e){System.out.println(e);}    
+	      }    
+
+}
